ae75cca3b8b7eb9397ebbe656a81b0e1ab0e3232
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2025-09-13 13:30:49 +0530

    add CityListWrapper for cities.json

---
+++ Weatherly.API/Models/Input/CityListDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Weatherly.API.Models.Input
+{
+    public class CityListWrapper
+    {
+        public List<CityCodeEntry> List { get; set; } = new();
+    }
+
+    public class CityCodeEntry
+    {
+        public string CityCode { get; set; } = string.Empty;
+    }
+}
