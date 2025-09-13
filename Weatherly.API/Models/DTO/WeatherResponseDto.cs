89b6372ddbb9982add08ab79bfdf55b7aab87b6c
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2025-09-13 11:54:38 +0530

    Add WeatherResponseDto model for weather API responses

---
+++ Weatherly.API/Models/DTO/WeatherResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Weatherly.API.Models.DTO
+{
+    public class WeatherResponseDto
+    {
+        public string CityCode { get; set; }
+        public string CityName { get; set; }
+        public string Temp { get; set; }
+        public string Status { get; set; }
+    }
+}
